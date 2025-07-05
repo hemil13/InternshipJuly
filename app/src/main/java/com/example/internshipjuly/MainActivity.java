4214a582d883a84a61247e45802474f1436bda1e
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2025-07-05 17:46:24 +0530

    Toast, Snackbar

--- app/src/main/java/com/example/internshipjuly/MainActivity.java
+++ app/src/main/java/com/example/internshipjuly/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.internshipjuly;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+        signin = findViewById(R.id.main_signin);
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Enter Email");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Enter Password");
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Login Successful", Snackbar.LENGTH_LONG).show();
+                }
+            }
         });
+
     }
 }
